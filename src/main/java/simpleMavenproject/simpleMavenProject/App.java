5ed8abe41d5b3b934ef6d348564f88e609bd9bf0
kc-alapati <1113akc@gmail.com>
2024-02-29 11:56:59 +0530

    Initial commit

---
+++ src/main/java/simpleMavenproject/simpleMavenProject/App.java
@@ -0,0 +1,15 @@
+package simpleMavenproject.simpleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Welcome to Jenkins");
+        System.out.println("Extra testing Line");
+    }
+}
